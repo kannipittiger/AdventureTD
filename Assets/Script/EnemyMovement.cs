3d97357b342384700c12ca8f57663c0986ae5828
b6430300463 <nanthapong.l@ku.th>
2024-08-20 14:29:05 +0700

    update
    ตามนั้น

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+// using System.Numerics;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public bool facingRight;
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target;
+    private int pathIndex = 0;
+    private void Start(){
+        target =  LevelManager.main.path[pathIndex];
+    }
+    private void Update(){
+        if(Vector2.Distance(target.position, transform.position) <= 0.1f){
+            pathIndex++;
+            if(pathIndex == LevelManager.main.path.Length){
+                Destroy(gameObject);
+                return;
+            }else{
+                target =  LevelManager.main.path[pathIndex];
+            }
+        }
+        if(pathIndex >= 2 && pathIndex < 6){
+            facingRight = false;
+        }else if(pathIndex >= 6 && pathIndex < 7){
+            facingRight = true;
+        }else if(pathIndex >= 8){
+            facingRight = false;
+        }
+
+        if(facingRight){
+            transform.localScale = new Vector2(1,1);
+        }else{
+            transform.localScale = new Vector2(-1,1);
+        }
+        
+    }
+    private void FixedUpdate(){
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * moveSpeed;
+    }
+}
