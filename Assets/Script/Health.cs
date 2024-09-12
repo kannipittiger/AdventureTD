95d6b3e58347ab8e543a47b39f3b1aae688ed417
b6430300463 <nanthapong.l@ku.th>
2024-09-12 14:54:39 +0700

    Hero placement
    ตามนั้น

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -7,10 +7,13 @@ public class Health : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
 
+    private bool isDestroyed = false;
+
     public void TakeDamage(int dmg){
         hitPoints -= dmg;
-        if(hitPoints <= 0){
+        if(hitPoints <= 0 && !isDestroyed){
             EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
