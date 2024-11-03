b9036bb76c3e4b1ccd516004a6ca3e84babe918e
kannipittiger <kannipit.p@ku.th>
2024-11-04 00:56:30 +0700

    trtrtrt

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Menu : MonoBehaviour
@@ -8,11 +9,27 @@ public class Menu : MonoBehaviour
     [Header("References")]
     [SerializeField] TextMeshProUGUI curremcyUI;
 
+    public GameObject pauseMenuScreen;
+
     private void OnGUI(){
         curremcyUI.text = LevelManager.main.currency.ToString();
     }
 
     public void SetSelected(){
         
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 0;
+        pauseMenuScreen.SetActive(true);
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1;
+        pauseMenuScreen.SetActive(false);
+    }
+
+    public void GotoMenu(){
+        SceneManager.LoadScene("start game");
     }
 }
